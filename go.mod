d1dd1f6514899ed6eed46bdb69b31d67e5a0f96f
Cristian Cercel <cristian.cercel@outlook.com>
2026-09-17 20:41:27 -0400

    schemas done for now

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/cscercel/grimoire
 
 go 1.27.1
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
--- sql/migrations/00001_init_schema.sql
+++ sql/migrations/00001_init_schema.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE users (
-    id              TEXT        PRIMARY KEY DEFAULT,
+    id              TEXT        PRIMARY KEY,
     email           TEXT        NOT NULL UNIQUE,
     hashed_password TEXT        NOT NULL,
     created_at      TEXT        NOT NULL DEFAULT (datetime('now')),
@@ -8,20 +8,42 @@ CREATE TABLE users (
 );
 
 
+CREATE TABLE clubs (
+    id          TEXT    PRIMARY KEY,
+    name        TEXT    NOT NULL UNIQUE,
+    admin_id    TEXT    REFERENCES users(id),
+    passkey     TEXT    NOT NULL,
+    created_at  TEXT    NOT NULL DEFAULT (datetime('now')),
+    updated_at  TEXT    NOT NULL DEFAULT (datetime('now'))
+)
+
+
+CREATE TABLE club_members (
+    id          TEXT    PRIMARY KEY,
+    username    TEXT    NOT NULL UNIQUE,
+    club_id     TEXT    REFERENCES clubs(id),
+    user_id     TEXT    REFERENCES users(id),
+    created_at  TEXT    NOT NULL DEFAULT (datetime('now')),
+    updated_at  TEXT    NOT NULL DEFAULT (datetime('now'))
+)
+
+
 CREATE TABLE books (
-    id        TEXT PRIMARY KEY,
-    isbn      TEXT NOT NULL UNIQUE,
-    title     TEXT NOT NULL,
-    author    TEXT NOT NULL,
-    publisher TEXT NOT NULL,
-    subject   TEXT NOT NULL,
+    id          TEXT    PRIMARY KEY,
+    isbn        TEXT    NOT NULL UNIQUE,
+    title       TEXT    NOT NULL,
+    author      TEXT    NOT NULL,
+    publisher   TEXT    NOT NULL,
+    subject     TEXT    NOT NULL,
+    created_at  TEXT    NOT NULL DEFAULT (datetime('now')),
+    updated_at  TEXT    NOT NULL DEFAULT (datetime('now'))
 );
 
 
-CREATE TABLE shelves (
+CREATE TABLE club_member_books (
     id          INTEGER     PRIMARY KEY AUTOINCREMENT,
-    user_id     TEXT        REFERENCES(user.id),
-    book_id     TEXT        REFERENCES(books.id),
+    member_id   TEXT        REFERENCES club_members(id),
+    book_id     TEXT        REFERENCES books(id),
     status      TEXT        NOT NULL DEFAULT 'tbr',
     rating      INTEGER,
     review      TEXT,
@@ -33,5 +55,7 @@ CREATE TABLE shelves (
 
 -- +goose Down
 DROP TABLE users;
+DROP TABLE clubs;
+DROP TABLE club_members;
 DROP TABLE books;
-DROP TABLE shelves;
+DROP TABLE club_member_books;
